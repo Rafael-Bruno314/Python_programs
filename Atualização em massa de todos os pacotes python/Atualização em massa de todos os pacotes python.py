7f8224b064628f690b3e454b14ec0dec3484ea45
Rafael Bruno <rafaelbruno.cunha@gmail.com>
2026-07-11 01:57:56 -0300

    update 'Atualização em massa de todos os pacotes python.py'

--- Atualização em massa de todos os pacotes python/Atualização em massa de todos os pacotes python.py
+++ Atualização em massa de todos os pacotes python/Atualização em massa de todos os pacotes python.py
@@ -1,2 +1,9 @@
+import subprocess
 from importlib.metadata import distributions
+
 packages = [dist.metadata['Name'] for dist in distributions()]
+
+for pkg in packages:
+    subprocess.run(['pip', 'install', '--upgrade', pkg])
+    
+"""pip list --outdated --format=json | python -c "import json, sys; from subprocess import call; [call(['pip', 'install', '--upgrade', pkg['name']]) for pkg in json.load(sys.stdin)]"""
